cd41119c42f828a1c0afe77840361b05a6d171bc
ptemple <patrick_temple@brown.edu>
2011-10-08 12:54:58 -0400

    changed to port 80

--- index.js
+++ index.js
@@ -83,5 +83,5 @@ function sendEmails() {
 }
 
 scheduleMessage();
-app.listen(8080);
+app.listen(80);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
