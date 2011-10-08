108d9e65289849ea3fb91826f69b56fec4843278
ptemple <patrick_temple@brown.edu>
2011-10-08 12:58:15 -0400

    switched back to 8080

--- index.js
+++ index.js
@@ -83,5 +83,5 @@ function sendEmails() {
 }
 
 scheduleMessage();
-app.listen(80);
+app.listen(8080);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
